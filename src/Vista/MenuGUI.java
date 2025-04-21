7f58687b22a7edc28566b6a89c486064a851b94c
USUARIO <nandolonga43@gmail.com>
2025-04-20 21:36:39 -0500

    Actualizacion Creacion del MenuGUI

---
+++ src/Vista/MenuGUI.java
@@ -0,0 +1,42 @@
+package Vista;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuGUI
+{
+    private JPanel main;
+    private JButton añadirEmpleadoButton;
+    private JButton tomarOrdenButton;
+    private JButton añadirMesaButton;
+    private JButton añadirProductoButton;
+    private JButton añadirClienteButton;
+    private JButton chatButton;
+    private JButton reportesButton;
+
+    public MenuGUI() {
+        tomarOrdenButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                //OrdenGUI ordenGUI = new OrdenGUI();
+                //ordenGUI.ejecutar();
+            }
+        });
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("CRUD Clientes");
+        frame.setContentPane(new MenuGUI().main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(800,600);
+        frame.setResizable(false);
+    }
+
+
+}
